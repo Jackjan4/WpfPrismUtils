f5d086a1d75d7f1bcdca18356854de8c8f69640d
Jan <Jackjan@protonmail.com>
2019-06-28 14:06:18 +0200

    Fixed Unity dependencies

--- PrismUtils/Properties/AssemblyInfo.cs
+++ PrismUtils/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ROSLAN")]
 [assembly: AssemblyProduct("WpfPrismUtils")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Licensed under MIT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
